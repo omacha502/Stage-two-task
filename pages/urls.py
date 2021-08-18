503a90b310db3f97f9b77f3f6e9a2c7c3c37d136
Patrick <patrickoluwafor@gmail.com>
2021-08-18 22:14:56 +0100

    fixed the contact form page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from .views import HomePageView
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', views.index, name='index'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,28 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from .models import Contact
+from django.http import HttpResponse
 
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+def index(request):
+    return render(request, 'index.html')
+
+
+def index(request):
+    if request.method == "POST":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        contact.name = name
+        contact.email = email
+        contact.subject = subject
+        contact.message = message
+        contact.save()
+        return HttpResponse('<h1>Thank you for submitting</h1>'
+                            'My Stage two task Submission'
+                            'The contact form responses are stored in the django database,'
+                            'you will have to login to the django admin dashboard to see responses'
+                            '')
+    return render(request, 'index.html')
